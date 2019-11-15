b1f9fbddad53b9022847b8164da3f3907a940f6c
Sérgio Alonso Jr <SergioAlonsoJunior94@gmail.com>
2019-11-15 11:12:45 -0300

    Escrever criador de .txt com palavras aleatórias

---
+++ biblioteca_textos/criaBiblioteca.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"path/filepath"
+	"strconv"
+	"time"
+)
+
+var scanner *bufio.Scanner
+var file *os.File
+var listOfWords []string = make([]string, 330000)
+var wordCount int
+var folderCount int = 1
+var fileCount int = 1
+
+func main() {
+	openFile()
+
+	dir := createDirectory()
+	createTextFile(100, dir)
+}
+
+func openFile() {
+	file, err := os.Open("." + string(filepath.Separator) + "palavras_fonte.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner = bufio.NewScanner(file)
+	for scanner.Scan() {
+		listOfWords[wordCount] = scanner.Text()
+		wordCount++
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(listOfWords), func(i, j int) { listOfWords[i], listOfWords[j] = listOfWords[j], listOfWords[i] })
+
+	fmt.Println("Finished creating array")
+}
+
+func getNextWord() string {
+	if len(listOfWords) <= 0 {
+		openFile()
+	}
+	newWord := ""
+	for newWord == "" {
+		randomIndex := rand.Intn(len(listOfWords))
+
+		newWord = listOfWords[randomIndex]
+		listOfWords = remove(listOfWords, randomIndex)
+	}
+	fmt.Println(newWord)
+	return newWord
+}
+
+func createTextFile(numberOfWords int, folderName string) {
+	slash := string(filepath.Separator)
+	dir := folderName + slash + "livro-" + strconv.Itoa(fileCount) + ".txt"
+	fmt.Println(dir)
+	file, err := os.Create(dir)
+	defer file.Close()
+	check(err)
+
+	fileCount++
+
+	w := bufio.NewWriter(file)
+	for numberOfWords > 0 {
+		w.WriteString(getNextWord() + "\n")
+		numberOfWords--
+	}
+
+	w.Flush()
+}
+
+func createDirectory() string {
+	folderName := "pasta" + strconv.Itoa(folderCount)
+	folderCount++
+	dir := "." + string(filepath.Separator) + folderName
+	os.Mkdir(dir, 0777)
+	//fmt.Println(dir)
+	return dir
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func remove(s []string, i int) []string {
+	s[len(s)-1], s[i] = s[i], s[len(s)-1]
+	return s[:len(s)-1]
+}
